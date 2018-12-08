c38dad353814ed6d668cfabff98a24237d344173
Nicolaudie <adam.smith@nicolaudiegroup.com>
2018-12-08 14:00:18 +0000

    First Commit

---
+++ Gutted/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gutted
+//
+//  Created by Adam Rikardsen-Smith on 08/12/2018.
+//  Copyright © 2018 Adam Rikardsen-Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
