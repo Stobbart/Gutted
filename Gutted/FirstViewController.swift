e652bbf391bbb9ff2d57052b92001c3c6afb939e
Nicolaudie <adam.smith@nicolaudiegroup.com>
2018-12-08 14:02:06 +0000

    new

---
+++ Gutted/FirstViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FirstViewController.swift
+//  Gutted
+//
+//  Created by Adam Rikardsen-Smith on 08/12/2018.
+//  Copyright © 2018 Adam Rikardsen-Smith. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
