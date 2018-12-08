ce6abe4a70f8f666beb123b337458bf0eb14fa67
Nicolaudie <adam.smith@nicolaudiegroup.com>
2018-12-08 14:06:27 +0000

    b=new

---
+++ Gutted/New2ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  New2ViewController.swift
+//  Gutted
+//
+//  Created by Adam Rikardsen-Smith on 08/12/2018.
+//  Copyright © 2018 Adam Rikardsen-Smith. All rights reserved.
+//
+
+import UIKit
+
+class New2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
